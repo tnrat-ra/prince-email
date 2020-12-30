5a423a64d6e43ad22029468b3d0435ade2447262
tnrat-ra <75009328+tnrat-ra@users.noreply.github.com>
2020-12-31 01:47:01 +0530

    Add files via upload

---
+++ prince-email.sh
@@ -0,0 +1,15 @@
+pkg install nodejs
+npm install -g bash-obfuscate
+pkg install figlet 
+pkg install toilet
+cd
+cd prince-email
+mv email.sh prince_email.apk
+cp -r prince_email.apk $HOME
+cd
+figlet Prince Email |lolcat|lolcat
+cd
+mv -v prince_email.apk /sdcard
+toilet Prince Yasin |lolcat|lolcat
+echo -e "\e[1;34m[*] \e[32mPlease wait....setuping\e[0m";
+echo -e "\e[1;34m[*] \e[32mCheck your sdcard (internal storage) and install prince_email.apk\e[0m";
